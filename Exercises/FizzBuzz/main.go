cb29612675aca3dc09946396cd530cc9b509a457
booleanalgebra <davidgodin@David-Godins-MacBook-Pro-3.local>
2017-03-28 15:29:19 -0700

    FizzBuzz Mod
    modified FizzBuzz for brevity and to test GitHub.  Removed %3 and 5 and
    added 15.

--- Exercises/FizzBuzz/main.go
+++ Exercises/FizzBuzz/main.go
@@ -5,9 +5,9 @@ import "fmt"
 func main(){
 	for i :=0; i<=100; i++  {
 		switch {
-		case (i % 3 == 0) && (i % 5 !=0): fmt.Println("Fizz")
-		case (i % 5 == 0) && (i % 3 != 0): fmt.Println("Buzz")
-		case (i % 3 == 0) && (i % 5 == 0): fmt.Println("FizzBuzz")
+		case (i % 3 == 0) && (i % 5 !=0): fmt.Println(i, "- Fizz")
+		case (i % 5 == 0) && (i % 3 != 0): fmt.Println(i, "- Buzz")
+		case (i % 15 == 0): fmt.Println(i, "- FizzBuzz")
 		default: fmt.Println(i)
 
 
--- Exercises/Summation_3_5_Multiples/main.go
+++ Exercises/Summation_3_5_Multiples/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main(){
-	var z int
-	for i := 0; i<1000; i++  {
+	var z int64
+	var i int64
+	for i = 0; i<1000; i++  {
 		if i%3==0 {z += i
 
 		} else if i%5==0 {z += i
